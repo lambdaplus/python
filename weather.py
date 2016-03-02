1f5df711abc926ca8f61736f68bae677905d9518
lambdaplus <bond010418@gmail.com>
2016-03-02 09:08:56 +0800

    modify weather.py

--- weather.py
+++ weather.py
@@ -6,19 +6,19 @@ import json
 import requests
 
 def get_weather(loaction):
-    
+
     response = requests.get(url)
     data = response.json()
     weather = data['result']
     keys = ['week', 'days', 'weather', 'temperature', 'winp']
-    
+
     print('>>>未来%s七天天气如下<<<\n' %weather[0]['citynm'])
     for day in range(len(weather)):
         for key in keys:
             print(weather[day][key])
         print('---------------')
 
-if __name__ == '__main__': 
+if __name__ == '__main__ True:
     while True:
         loaction = input('请输入你想要查询的城市名称，例如：北京或beijing\n>>>')
         if loaction == 'q':
