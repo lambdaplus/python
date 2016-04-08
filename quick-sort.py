f80fb7daa4d90acd3df95a7ec624de5af64f6dec
lambdaplus <bond010418@gmail.com>
2016-04-08 22:54:18 +0800

    modify quick-sort

--- quick-sort.py
+++ quick-sort.py
@@ -1,14 +1,14 @@
 # coding=utf-8
-
+import random
 
 def quick_sort(L):
     if L == []:
         return L
-    mid = L[0]
+    mid = random.choice(L)
     small = [x for x in L if x < mid]
-    big = [x for x in L if x >= mid]
-    return small + [mid] + big
+    big = [x for x in L if x > mid]
+    return quick_sort(small) + [mid] + quick_sort(big)
 
 if __name__ == '__main__':
-    L = [23, 1, 45, 3, 6, 89, 345, 8, 345]
+    L = [23, 3, 45, 1, 6, 89, 345, 8, 345]
     print(quick_sort(L))
