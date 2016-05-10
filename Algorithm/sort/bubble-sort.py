f7891b9733ec578c5fddbde1a894d0075adc28fa
lambdaplus <bond010418@gmail.com>
2016-05-10 09:50:33 +0800

    modify sort

--- Algorithm/sort/bubble-sort.py
+++ Algorithm/sort/bubble-sort.py
@@ -3,7 +3,10 @@ from random import randrange
 
 
 def bubble_sort(L):
-    if len(L) < 2:
+    '''
+    冒泡排序主要使用两次循环实现排序。外循环中的一个数字依次与内层循环中的每个数字进行比较，如果索引值小的数字大于索引值大的数字，交换位置。否则，不变。直至循环结束
+    '''
+    if len(L) < 2: # 列表内元素低于2，直接返回！
         return L
     for i in range(len(L)):
         for j in range(1, len(L)):
--- Algorithm/sort/quick-sort.py
+++ Algorithm/sort/quick-sort.py
@@ -1,14 +1,15 @@
 # coding=utf-8
 import random
 
-def quick_sort(L):
-    if L == []:
-        return L
-    mid = random.choice(L)
-    small = [x for x in L if x < mid]
-    big = [x for x in L if x > mid]
+def quick_sort(seq):
+    if len(seq) < 2:
+        return seq
+
+    mid = random.choice(seq)
+    small = [x for x in seq if x < mid]
+    big = [x for x in seq if x > mid]
     return quick_sort(small) + [mid] + quick_sort(big)
 
 if __name__ == '__main__':
-    L = [23, 3, 45, 1, 6, 89, 345, 8, 345]
+    L = [random.randrange(1000) for _ in range(10)]
     print(quick_sort(L))
