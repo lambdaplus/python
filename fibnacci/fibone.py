8af0441c124796c7aa62dfa7d22c03bdcfe4910e
lambdaplus <bond010418@gmail.com>
2016-05-13 13:52:17 +0800

    add fibonacci

---
+++ fibnacci/fibone.py
@@ -0,0 +1,36 @@
+斐波那契数列的几种算法
+
+1. 普通的循环
+```python
+# -*- coding: utf-8 -*-
+
+def fib(n):
+    lst = []
+    m, a, b = 0, 0, 1
+    for m in range(n):
+        lst.append(a)
+        a, b = b, a + b
+
+    return lst
+
+if __name__ == "__main__":
+    n = int(input('Please input n > '))
+    print(fib(n))
+```
+
+2. 高效的生成器
+```python
+def fib(n):
+    m, a, b = 0, 0, 1
+    for m in range(n):
+        yield a
+        a, b = b, a + b
+
+if __name__ == '__main__':
+    lst = []
+    n = int(input('Please input n > '))
+    for i in fib(n):
+        lst.append(i)
+    print(lst)
+```
+## 当然还有各种lambda奇淫技巧，不写了！
